ff1ecc5fae2aa1614b9dd465c671b9a3eef92b89
WoodHolz <1535980162@qq.com>
2023-06-27 12:16:04 +0800

    display num loop

--- lighting_1.asm
+++ lighting_1.asm
@@ -8,13 +8,15 @@ MY8255_MODE EQU IOYO + 03H*2
 ;DSEG ENDS
 
 STACK SEGMENT STACK
-    ;DW 100H DUP(?)
+    DB 100 DUP(?)
 STACK ENDS
 CODE SEGMENT
     ASSUME CS: CODE;, DS: DSEG
 START:
-;    MOV AX, DSEG
-;    MOV DS, AX
+    ;MOV AX, DSEG
+    ;MOV DS, AX
+
+
 
     MOV AL, 10000000B
     MOV DX, MY8255_MODE
@@ -23,10 +25,82 @@ START:
 ;    MOV BX, OFFSET TAL
 ;    INC BX
 ;    MOV AL, [BX]
-    MOV AL, 6DH
-    MOV DX, MY8255_B
-    OUT DX, AL
 
+    ;9
+    call DELAY
+    ;RET_DELAY:
+    mov al, 6fH
+    mov dx, MY8255_B
+    out dx, al
+
+;8
+    call DELAY
+    ;RET_DELAY:
+    mov al, 7fH
+    mov dx, MY8255_B
+    out dx, al
+;7
+    call DELAY
+    ;RET_DELAY:
+    mov al, 07H
+    mov dx, MY8255_B
+    out dx, al
+;6
+    call DELAY
+    ;RET_DELAY:
+    mov al, 7dH
+    mov dx, MY8255_B
+    out dx, al
+;5
+    call DELAY
+    ;RET_DELAY:
+    mov al, 6dH
+    mov dx, MY8255_B
+    out dx, al
+;4
+    call DELAY
+    ;RET_DELAY:
+    mov al, 66H
+    mov dx, MY8255_B
+    out dx, al
+;3
+    call DELAY
+    ;RET_DELAY:
+    mov al, 4fH
+    mov dx, MY8255_B
+    out dx, al 
+;2
+    call DELAY
+    ;RET_DELAY:
+    mov al, 5BH
+    mov dx, MY8255_B
+    out dx, al
+;1
+    call DELAY
+    ;RET_DELAY:
+    mov al, 06H
+    mov dx, MY8255_B
+    out dx, al
+;0
+    call DELAY
+    ;RET_DELAY:
+    mov al, 3fH
+    mov dx, MY8255_B
+    out dx,al
+    
+    jmp ENDG
+    
+    DELAY:
+    ;PUSH CX
+    MOV CX,0ffffh
+    LOOP_d:
+    DEC CX 
+    CMP CX, 0
+    JGE LOOP_d
+    ;POP CX
+    RET
+    
+    ENDG:
     MOV AH, 4CH
     INT 21H
 CODE ENDS
